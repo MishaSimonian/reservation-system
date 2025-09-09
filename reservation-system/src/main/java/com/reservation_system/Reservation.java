cbeb2cedad6b7ed0a13f03206c4eabfa7d41415d
Misha Simonia <simonyanmishakareni@outlook.com>
2025-09-09 14:12:47 +0200

    feat: Add reservation system skeleton

---
+++ reservation-system/src/main/java/com/reservation_system/Reservation.java
@@ -0,0 +1,7 @@
+package com.reservation_system;
+
+import java.time.LocalDate;
+
+public record Reservation(Long id, Long userId, Long roomId, LocalDate startDate, LocalDate endDate, ReservationStatus status) {
+
+}
